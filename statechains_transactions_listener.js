8d82318bf27aa863a65f500e97306f7fe2eef166
RajKadir <r.kadir@se16.qmul.ac.uk>
2022-03-07 23:33:20 +0000

    chore: add checks for undefined

--- statechains_transactions_listener.js
+++ statechains_transactions_listener.js
@@ -2,22 +2,23 @@ const { removeNullValues, encodeSCEAddress } = require('./utils/utils.js')
 const {  get, updateOne } = require('./utils/getReqs.js')
 const { functionOnDBs } = require('./utils/dbConnection.js')
 const { STMigration } = require('./utils/st_utils.js')
-const {getConfig} = require('./utils/config.js')
+const { getConfig } = require('./utils/config.js')
 
-const libraryNotFound = (lib) => {
+const checkNotUndefined = () => {
 	if(lib === undefined || lib === null){
 		throw Error(`${lib} not found`);
 	}
 }
 
+// mongodb config
 var MongoClient = require('mongodb').MongoClient;
-libraryNotFound(MongoClient);
+checkNotUndefined(MongoClient);
 
 const pg =  require('pg');
-libraryNotFound(pg);
+checkNotUndefined(pg);
 
-const CONFIG = getConfig()
-libraryNotFound(CONFIG);
+const CONFIG = getConfig();
+checkNotUndefined(CONFIG);
 
 functionOnDBs( STMigration )
 
@@ -27,8 +28,7 @@ var client = new pg.Client(CONFIG.mercuryDb).connect(async function(err, client)
     console.log(err);
   }
 
-  // debug what client is
-  console.log(client);
+  console.log('client value is: ', client);
   
   //LISTEN to statechain table
   var query = client.query("LISTEN statechain_changed")
@@ -116,4 +116,3 @@ var client = new pg.Client(CONFIG.mercuryDb).connect(async function(err, client)
 
 	
 })
-		

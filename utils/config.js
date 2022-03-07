4241c0009ed1d554662777e529da7942823e90ec
Sam Gershuny <samgershuny@Lawrences-MacBook-Pro-4.local>
2022-03-07 10:05:05 +0000

    add mercury db details

--- utils/config.js
+++ utils/config.js
@@ -21,7 +21,13 @@ function getConfig(){
 		let config = {
 			explorerURI: process.env.EXPLORER_URI,
 			mercuryAPI: process.env.TESTNET_MERCURY_API,
-			mercuryDb: process.env.TESTNET_MERCURY_DB,
+			mercuryDb: {
+				"user": process.env.TEST_MERCURY_DB_USER,
+				"host": process.env.TEST_MERCURY_DB_HOST,
+				"database": process.env.TEST_MERCURY_DB_NAME,
+				"password": process.env.TEST_MERCURY_DB_PASS,
+				"port": process.env.TEST_MERCURY_DB_PORT
+		   },
 			dbName: process.env.DB_NAME,
 			statechains: process.env.TESTNET_STATECHAINS,
 			transactions: process.env.TESTNET_TRANSACTIONS,
@@ -34,7 +40,13 @@ function getConfig(){
 		let config = {
 			explorerURI: process.env.EXPLORER_URI,
 			mercuryAPI: process.env.MAINNET_MERCURY_API,
-			mercuryDb: process.env.MAINNET_MERCURY_DB,
+			mercuryDb: {
+				"user": process.env.MERCURY_DB_USER,
+				"host": process.env.MERCURY_DB_HOST,
+				"database": process.env.MERCURY_DB_NAME,
+				"password": process.env.MERCURY_DB_PASS,
+				"port": process.env.MERCURY_DB_PORT
+		   },
 			dbName: process.env.DB_NAME,
 			statechains: process.env.MAINNET_STATECHAINS,
 			transactions: process.env.MAINNET_TRANSACTIONS,
@@ -43,3 +55,16 @@ function getConfig(){
 		return config
 	}
 }
+
+
+EXPLORER_URI =  "mongodb://Mercury:Dux7MYeVff8xhnLt@108.61.117.115:27017/?retryWrites=true&w=majority"
+TESTNET_MERCURY_API = "http://78.141.223.118:32452"
+TEST_MERCURY_DB_USER = "mercury"
+TEST_MERCURY_DB_HOST = "psql.mercurywallet.io"
+TEST_MERCURY_DB_NAME = "test7"
+TEST_MERCURY_DB_PASS = "3c6CJe4rg8My8j"
+TEST_MERCURY_DB_PORT = 5434
+DB_NAME = "notifications"
+TESTNET_STATECHAINS = "statechains_tests"
+TESTNET_TRANSACTIONS = "transactions_tests"
+TESTNET_BATCH_TRANSFERS = "batchtransfers_tests"

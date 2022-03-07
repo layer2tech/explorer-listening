fcae4e08b3e49ed477c232732e587114241ac7c3
Sam Gershuny <samgershuny@Lawrences-MacBook-Pro-4.local>
2022-03-07 10:05:40 +0000

    add mercury db details

--- utils/config.js
+++ utils/config.js
@@ -55,16 +55,3 @@ function getConfig(){
 		return config
 	}
 }
-
-
-EXPLORER_URI =  "mongodb://Mercury:Dux7MYeVff8xhnLt@108.61.117.115:27017/?retryWrites=true&w=majority"
-TESTNET_MERCURY_API = "http://78.141.223.118:32452"
-TEST_MERCURY_DB_USER = "mercury"
-TEST_MERCURY_DB_HOST = "psql.mercurywallet.io"
-TEST_MERCURY_DB_NAME = "test7"
-TEST_MERCURY_DB_PASS = "3c6CJe4rg8My8j"
-TEST_MERCURY_DB_PORT = 5434
-DB_NAME = "notifications"
-TESTNET_STATECHAINS = "statechains_tests"
-TESTNET_TRANSACTIONS = "transactions_tests"
-TESTNET_BATCH_TRANSFERS = "batchtransfers_tests"

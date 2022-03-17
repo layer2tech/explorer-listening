620f85c7378c15b676e3328d70ff31ea9a6bfe68
Sam Gershuny <samgershuny@Lawrences-MacBook-Pro-4.local>
2022-03-17 13:13:55 +0000

    SQL Query in place of Get Reqs

--- main_bt/batchtransfer_listener.js
+++ main_bt/batchtransfer_listener.js
@@ -1,4 +1,4 @@
-const { get, insertOne } = require('./utils/getReqs.js') 
+const { insertOne } = require('./utils/getReqs.js') 
 
 const { functionOnDBs } = require('./utils/dbConnection.js')
 const { BTMigration } = require('./utils/bt_utils.js')
@@ -7,10 +7,12 @@ const { getConfig } = require('./utils/config.js')
 // mongodb config
 var MongoClient = require('mongodb').MongoClient;
 const pg =  require('pg');
+const { getBatchTransferById, sortBatchTransfers } = require('./utils/utils.js');
 
 const CONFIG = getConfig()
 
 functionOnDBs(BTMigration)
+// Update data with entire DB
 
 console.log("config: ".CONFIG)
 
@@ -33,19 +35,20 @@ var client = new pg.Client(CONFIG.mercuryDb).connect(async function(err, client)
 			var id = JSON.parse(msg.payload).record
 		}
 		try{
-			let batchtransfer = await get(CONFIG.mercuryAPI,'info/transfer-batch', id)
+			console.log('Batch Tx ID Record updated: ',id)
 
-			let batchTransferEntry = {
-				batch_id: id,
-				statechains: batchtransfer.state_chains,
-				finalized_at: Date.now()
-			}
-			console.log('statechains: ',batchTransferEntry.statechains)
+			console.log('Querying DB...')
+			let data = await getBatchTransferById(client,id)
+			// Query DB for Batch Transfer Data
+
+			console.log('Sorting Data...')
+			const btArray = sortBatchTransfers(data.rows)
+			// Sort data for explorer db
 			
-  			if(batchtransfer.finalized){
-  				// Enter new batch transfer entry
-                    		await insertOne(db, CONFIG.dbName, CONFIG.batchtransfers, batchTransferEntry, true)
-                    		
+			if(btArray[0]){
+				console.log('INSERTED Swap: ',btArray[0])
+				await insertOne(db, CONFIG.dbName, CONFIG.batchtransfers, btArray[0], true)
+				// insert btArray[0] into explorer DB
 			}
   						
 			}
